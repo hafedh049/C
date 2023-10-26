90534bb18134f8a3a1e0af261f63434ddc393170
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-10-26 18:31:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,6 @@
-#include "string/string.c"
+//#include ""
 
 int main() {
-    char* originalStr = "Hello, World!";
-    String* str = createString(originalStr);
-
-    printf("Original String: %s\n", originalStr);
-    printf("Created String: %s\n", str->str);
-    printf("String Length: %d\n", str->length);
-    char* words[] = { "Hello", "World", "OpenAI", "GPT-3" };
-    int numWords = sizeof(words) / sizeof(words[0]);
-
-    String** strings = (String**)malloc(numWords * sizeof(String*));
-    for (int i = 0; i < numWords; i++) {
-        strings[i] = createString(words[i]);
-    }
-    printf("%s\n", join(createString("||"),strings,numWords)->str);
+    
     return 0;
 }
