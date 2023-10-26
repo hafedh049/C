ae8aefa18277d754a2bb1ceea9af29cd81b06466
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-10-26 16:32:52 +0100

    Update circular_linked_list.c

--- circular_linked_list/circular_linked_list.c
+++ circular_linked_list/circular_linked_list.c
@@ -113,11 +113,13 @@ int search(CircularLinkedList *list, int element)
   return -1;
 }
 
-int getLength(CircularLinkedList *list) {
+int getLength(CircularLinkedList *list)
+{
   int length = 0;
   const Node *current = list->head;
 
-  while (current != list->head) {
+  while (current != list->head)
+  {
     length++;
     current = current->next;
   }
@@ -125,12 +127,14 @@ int getLength(CircularLinkedList *list) {
   return length;
 }
 
-void reverse(CircularLinkedList *list) {
+void reverse(CircularLinkedList *list)
+{
   Node *current = list->head;
   Node *prev = NULL;
   Node *next = NULL;
 
-  while (current != list->head) {
+  while (current != list->head)
+  {
     next = current->next;
     current->next = prev;
     prev = current;
@@ -140,11 +144,13 @@ void reverse(CircularLinkedList *list) {
   list->head = prev;
 }
 
-void rotate(CircularLinkedList *list, int k) {
+void rotate(CircularLinkedList *list, int k)
+{
   Node *current = list->head;
   int i = 0;
 
-  while (i < k && current != list->head) {
+  while (i < k && current != list->head)
+  {
     current = current->next;
     i++;
   }
@@ -155,4 +161,181 @@ void rotate(CircularLinkedList *list, int k) {
   list->head = current->next;
   current->next = list->head->next;
   list->head->next = current;
+}
+
+Node *findMiddleElement(CircularLinkedList *list)
+{
+  Node *current = list->head;
+  Node *previous = NULL;
+
+  while (current->next != list->head)
+  {
+    previous = current;
+    current = current->next;
+  }
+
+  return previous;
+}
+
+Node *findKthElement(CircularLinkedList *list, int k)
+{
+  int i = 0;
+  Node *current = list->head;
+
+  while (i < k && current != list->head)
+  {
+    current = current->next;
+    i++;
+  }
+
+  if (current == list->head)
+    return NULL;
+
+  return current;
+}
+
+CircularLinkedList *mergeCircularLinkedLists(CircularLinkedList *list1, CircularLinkedList *list2)
+{
+  CircularLinkedList *mergedList = createCircularLinkedList();
+
+  Node *current1 = list1->head;
+  Node *current2 = list2->head;
+
+  while (current1 != list1->head || current2 != list2->head)
+  {
+    if (current1 == list1->head)
+    {
+      insertAtHead(mergedList, current2->data);
+      current2 = current2->next;
+    }
+    else if (current2 == list2->head)
+    {
+      insertAtHead(mergedList, current1->data);
+      current1 = current1->next;
+    }
+    else
+    {
+      if (current1->data < current2->data)
+      {
+        insertAtHead(mergedList, current1->data);
+        current1 = current1->next;
+      }
+      else
+      {
+        insertAtHead(mergedList, current2->data);
+        current2 = current2->next;
+      }
+    }
+  }
+
+  return mergedList;
+}
+
+int isPalindrome(CircularLinkedList *list)
+{
+  Node *current = list->head;
+  Node *opposite = findMiddleElement(list);
+
+  while (current != opposite)
+  {
+    if (current->data != opposite->data)
+      return 0;
+
+    current = current->next;
+    opposite = opposite->next->next;
+  }
+
+  return 1;
+}
+
+void deleteNodeAtPosition(CircularLinkedList *list, int position)
+{
+  if (position < 0 || position >= getLength(list))
+    return;
+
+  int currentPosition = 0;
+  Node *current = list->head;
+  Node *previous = NULL;
+
+  while (current != list->head)
+  {
+    if (currentPosition == position)
+    {
+      if (previous == NULL)
+        list->head = current->next;
+      else
+        previous->next = current->next;
+
+      free(current);
+      break;
+    }
+
+    currentPosition++;
+    current = current->next;
+  }
+}
+
+void insertNodeAfterNode(CircularLinkedList *list, Node *node, int data)
+{
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = node->next;
+
+  node->next = newNode;
+}
+
+int findFirstOccurrence(CircularLinkedList *list, int element)
+{
+  int position = 0;
+  Node *current = list->head;
+
+  while (current != list->head)
+  {
+    if (current->data == element)
+      return position;
+
+    position++;
+    current = current->next;
+  }
+
+  return -1;
+}
+
+int findLastOccurrence(CircularLinkedList *list, int element)
+{
+  int position = -1;
+  Node *current = list->head;
+
+  while (current != list->head)
+  {
+    if (current->data == element)
+      position = getLength(list) - 1 - position;
+
+    position++;
+    current = current->next;
+  }
+
+  return position;
+}
+
+int hasCycle(CircularLinkedList *list)
+
+{
+  Node *fast = list->head;
+  Node *slow = list->head;
+
+  while (fast != NULL && fast->next != NULL)
+  {
+    fast = fast->next->next;
+    slow = slow->next;
+
+    if (fast == slow)
+    {
+      return
+
+          1;
+    }
+  }
+
+  return 0;
 }
